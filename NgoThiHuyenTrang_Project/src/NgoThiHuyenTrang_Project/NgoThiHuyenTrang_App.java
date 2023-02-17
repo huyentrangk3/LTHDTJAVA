e58f25d1ac10670946e0c7b7b2c59ef50405d45b
huyentrangk3 <125641787+huyentrangk3@users.noreply.github.com>
2023-02-17 15:01:26 +0700

    Add files via upload

---
+++ NgoThiHuyenTrang_Project/src/NgoThiHuyenTrang_Project/NgoThiHuyenTrang_App.java
@@ -0,0 +1,9 @@
+package NgoThiHuyenTrang_Project;
+
+public class NgoThiHuyenTrang_App {
+	public static void main(String[] args) {
+		
+		System.out.println("Ngô Thị Huyền Trang học Java");
+		
+	}
+}
